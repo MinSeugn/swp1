546399479f0e0c8730fa6cafbc5a13f770800a6a
MinSeugn <rlaalstmd0604@naver.com>
2020-06-26 14:32:33 +0900

    Issue #3 Make calc and move fig2.py, template2.py, fig2.conf into calc

---
+++ calc/template2.py
@@ -0,0 +1,13 @@
+html = b"""
+<html>
+    <body>
+        <form action="">
+            a = <input type="number" name="a"><br>
+            b = <input type="number" name="b"><br>
+            <input type="submit">
+        </form>
+        sum : %(sum)i<br>
+        product : %(product)i<br>
+    </body>
+</html>
+"""
